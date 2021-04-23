702a9baffa88c6574f615b527418f33a9d3e49cd
otavioscrp <otavioscrp@gmail.com>
2021-04-23 20:11:49 -0300

    Initial commit

---
+++ NavMeshStationStarter/Assets/SCRIPTS/AIControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//atribuir biblioteca de AI para a navegação
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    //definir variavel do objeto que realizará as ações do navmesh
+    public NavMeshAgent agent;
+
+    //ao iniciar o programa, atrivuir componente de navmesh para o objeto
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+}
+
---
+++ NavMeshStationStarter/Assets/SCRIPTS/AgentManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    //criação dos agentes no script de navmesh
+    GameObject[] agents;
+
+    //atribuir a Tag dos objetos que irão realizar a ação da navmesh 
+    //Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    //definição da ação apos o input do jogador com o botão do mouse
+    void Update()
+    {
+        //ao clique do mouse(botão zero)
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            //criação do raycast 
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach (GameObject a in agents)
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
